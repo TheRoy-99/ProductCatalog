56263ec41c9b9ed7704b74501268d1691f1877bd
TheRoy-99 <theroymrodriguez@gmail.com>
2026-02-23 21:08:24 -0500

    Configuracion de prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+  exports: [PrismaService],
 })
 export class AppModule {}
