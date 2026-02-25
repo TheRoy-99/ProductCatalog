80b11fa0c89a92e31295b548a500c778d00dbe98
Vicma98 <vhernandez@unac.edu.co>
2026-02-24 20:34:15 -0500

    feat(auth): implement user register with Prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,6 +20,30 @@ export class AuthService {
 
     // Quitamos el password del objeto antes de enviarlo al front por seguridad
     const { password, ...result } = user;
+    return result;
+  }
+  
+  async register(registerDto: RegisterDto) {
+    // Verificamos si el email ya está registrado
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: registerDto.email.toLowerCase() },
+    });
+
+    if (existingUser) {
+      throw new UnauthorizedException('El email ya esta registrado');
+    }
+
+    // Creamos el nuevo usuario
+    const newUser = await this.prisma.user.create({
+      data: {
+        name: registerDto.name,
+        email: registerDto.email.toLowerCase(),
+        password: registerDto.password,
+      },
+    });
+
+    // Quitamos el password del objeto antes de enviarlo al front por seguridad
+    const { password, ...result } = newUser;
     return result;
   }
 }
